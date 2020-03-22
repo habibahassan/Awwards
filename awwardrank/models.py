f1c20f0957f388cadef9a56074ee573fcf86ecce
habibahassan <halimaadan92@gmail.com>
2020-03-22 23:14:57 +0300

    added image model

--- awwardrank/models.py
+++ awwardrank/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.description
