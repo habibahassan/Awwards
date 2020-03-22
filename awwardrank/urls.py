d0ff05f9218504462bbeb82f867ee448b81a9a36
habibahassan <halimaadan92@gmail.com>
2020-03-22 23:29:21 +0300

    created register and login.html

--- awwardrank/urls.py
+++ awwardrank/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from .views import HomePageView
+from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import path,include
 
-urlpatterns = [
-   path('', HomePageView.as_view(), name='home'),
+
+
+urlpatterns=[
+    path('', views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('home/',views.home, name='home'),
+    path('project/(?P<project_id>[0-9])',views.project,name ='project'),
+    path('profile/',views.profile, name='profile'),
+    path('post/', views.upload_form, name='post'),
+    path('edit/', views.edit_prof, name='edit'),
+
 ]
