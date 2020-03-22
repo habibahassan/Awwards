1b497d7e8d1e7891bb1766daf0a659eb8de687fd
habibahassan <halimaadan92@gmail.com>
2020-03-22 23:13:29 +0300

    added urls

--- awward/urls.py
+++ awward/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+from awwardrank import views 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awwardrank.urls')),
+    # path('logout/', views.logout, {"next_page": '/'}),
+    # path('ratings/', include('star_ratings.urls',
+    #                           namespace='ratings', app_name='ratings')),
+    path('search/', views.search, name='search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
