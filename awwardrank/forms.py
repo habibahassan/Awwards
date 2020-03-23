742245c96c4d4f80d5005758a3ec06876dc5244a
habibahassan <halimaadan92@gmail.com>
2020-03-23 16:08:18 +0300

    added review form and comment form

--- awwardrank/forms.py
+++ awwardrank/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Projects, Profile,Review,Comments
 from django.core import validators
 
-
 class ProjectsForm(forms.ModelForm):
     class Meta:
         model = Projects
