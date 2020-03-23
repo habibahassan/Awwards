00ed582ad819f0ee134c9f1f1e77ecd53dda91f6
habibahassan <halimaadan92@gmail.com>
2020-03-23 16:08:58 +0300

    added view function for comment

--- awwardrank/views.py
+++ awwardrank/views.py
@@ -31,23 +31,23 @@ def project(request, project_id):
     except Projects.DoesNotExist:
         raise Http404()
 
-    comments = Review.get_comment(project_id)
-    latest_review_list=Review.objects.all().filter(project_id = project.id)
+    # comments = Review.get_comment(project_id)
+    # latest_review_list=Review.objects.all().filter(project_id = project.id)
 
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.project = project
-            review.user = request.user
-            review.save()
+    # if request.method == 'POST':
+    #     form = ReviewForm(request.POST)
+    #     if form.is_valid():
+    #         review = form.save(commit=False)
+    #         review.project = project
+    #         review.user = request.user
+    #         review.save()
 
-    else:
-        form = ReviewForm()   
+    # else:
+    #     form = ReviewForm()   
 
        
 
-    return render(request, 'project.html', {"project": project,'form':form,'comments':comments,'latest_review_list':latest_review_list})
+    # return render(request, 'project.html', {"project": project,'form':form,'comments':comments,'latest_review_list':latest_review_list})
 
     return render(request, "project.html", locals())
 
